08796505f77f08fa424f597373b4d55063a00f2d
Dia Saleh <diasaleh@Dias-MBP.home>
2016-01-06 23:07:16 +0200

    005 Adding animations on scroll

--- resources/js/script.js
+++ resources/js/script.js
@@ -41,5 +41,36 @@ $(document).ready(function () {
     }
   });
 });
+	/* animation on scroll */
+	$('.js--wp-1').waypoint(function(direction){
+		$('.js--wp-1').addClass('animated fadeIn');
+	}, {
+		offset: '50%'
+	});
+		$('.js--wp-2').waypoint(function(direction){
+		$('.js--wp-2').addClass('animated fadeInUp');
+	}, {
+		offset: '50%'
+	});
+			
+		$('.js--wp-3').waypoint(function(direction){
+		$('.js--wp-3').addClass('animated fadeIn');
+	}, {
+		offset: '50%'
+	});
+				
+		
+	$('.js--wp-4').waypoint(function(direction){
+		$('.js--wp-4').addClass('animated pulse');
+	}, {
+		offset: '50%'
+	});	
+	
+	$('.js--hello').waypoint(function(direction){
+		$('.js--hello').addClass('animated rotateInDownLeft');
+	},{
+		offset: '1000px'
+	}
+	);
 	
 });
